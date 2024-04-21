8a4d730de665d05a92d11ca98c53918a6bbb2c91
ak3036 <42751207+ak3036@users.noreply.github.com>
2024-04-21 10:56:22 +0530

    Modified the react-router-dom and added the navbar

--- src/App.js
+++ src/App.js
@@ -1,58 +1,14 @@
 import React from 'react';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { Navbar, Sidebar, Footer } from './components';
-import {
-  Home,
-  SingleProduct,
-  Cart,
-  Checkout,
-  Error,
-  About,
-  Products,
-  PrivateRoute,
-} from './pages';
 
 const App = () => {
-  const appRouter = createBrowserRouter([
-    {
-      path: '/',
-      element: <Home />,
-    },
-    {
-      path: '/about',
-      element: <About />,
-    },
-    {
-      path: '/cart',
-      element: <Cart />,
-    },
-    {
-      path: '/products',
-      element: <Products />,
-    },
-    {
-      path: '/products/:id',
-      element: <Home />,
-      children: [
-        {
-          element: <SingleProduct />,
-        },
-      ],
-    },
-    {
-      path: '/checkout',
-      element: <Checkout />,
-    },
-    {
-      path: '*',
-      element: <Error />,
-    },
-  ]);
   return (
     <div>
       <Navbar />
       <Sidebar />
-      <RouterProvider router={appRouter} />
+      <Outlet />
       <Footer />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import App from './App';
+
+import {
+  Home,
+  SingleProduct,
+  Cart,
+  Checkout,
+  Error,
+  About,
+  Products,
+  PrivateRoute,
+} from './pages/index';
 
 import { ProductsProvider } from './context/products_context';
 import { FilterProvider } from './context/filter_context';
@@ -9,6 +21,47 @@ import { CartProvider } from './context/cart_context';
 import { UserProvider } from './context/user_context';
 import { Auth0Provider } from '@auth0/auth0-react';
 
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '',
+        element: <Home />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+      {
+        path: '/cart',
+        element: <Cart />,
+      },
+      {
+        path: '/products',
+        element: <Products />,
+        children: [
+          {
+            path: '/products/:id',
+            element: <SingleProduct />,
+          },
+        ],
+      },
+
+      {
+        path: '/checkout',
+        element: <Checkout />,
+      },
+    ],
+  },
+
+  {
+    path: '*',
+    element: <Error />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<App />);
+root.render(<RouterProvider router={appRouter} />);
